2e5ccc6cd3e820724b3e98d74a3cc2401df95d0a
Diego Feitosa <dnfeitosa@gmail.com>
2015-10-04 18:34:49 -0700

    preventing null values to be returned by api responsed=s

--- codegraph-api/src/main/java/com/dnfeitosa/codegraph/web/resources/Resource/Resource.java
+++ codegraph-api/src/main/java/com/dnfeitosa/codegraph/web/resources/Resource/Resource.java
@@ -1,5 +1,10 @@
 package com.dnfeitosa.codegraph.web.resources.Resource;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@JsonInclude(NON_NULL)
 public interface Resource {
 
     String getUri();

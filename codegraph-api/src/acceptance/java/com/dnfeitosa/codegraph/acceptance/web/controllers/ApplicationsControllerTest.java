b2dc4494c20a2b9f1444159202d532b99fd76211
Diego Feitosa <dnfeitosa@gmail.com>
2015-10-04 19:30:34 -0700

    fixing unit test

--- codegraph-api/src/acceptance/java/com/dnfeitosa/codegraph/acceptance/web/controllers/ApplicationsControllerTest.java
+++ codegraph-api/src/acceptance/java/com/dnfeitosa/codegraph/acceptance/web/controllers/ApplicationsControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasItems;
 import static org.junit.Assert.assertThat;
@@ -21,11 +22,7 @@ public class ApplicationsControllerTest extends DatabaseDependentTest {
     public void shouldReturnAllApplications() {
         List<ApplicationResource> applications = controller.applications();
 
-        ApplicationResource vraptor = new ApplicationResource("vraptor4");
-
-        ApplicationResource mirror = new ApplicationResource("mirror");
-
         assertThat(applications.size(), is(2));
-        assertThat(applications, hasItems(vraptor, mirror));
+        assertThat(applications.stream().map(a -> a.getName()).collect(toList()), hasItems("vraptor4", "mirror"));
     }
 }

b8d1aa4474327c65c99a5a5321a63beab03520ae
Diego Feitosa <dnfeitosa@gmail.com>
2016-09-25 17:46:45 -0700

    changing the the gradle plugin extension class from groovy to java so it can be used with multiple gradle versions (1.x vs 2.x)

---
+++ codegraph-gradle-plugin/src/main/groovy/com/dnfeitosa/codegraph/gradle/plugin/extensions/CodegraphExtension.java
@@ -0,0 +1,14 @@
+package com.dnfeitosa.codegraph.gradle.plugin.extensions;
+
+public class CodegraphExtension {
+
+    private String url;
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}

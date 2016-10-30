88bd7d187a4deaed08f55bcf3a8cb17b968567c0
Diego Feitosa <dnfeitosa@gmail.com>
2016-10-29 18:38:41 -0700

    adding 'clean' task to gradle build

--- codegraph-client-base/src/main/java/com/dnfeitosa/codegraph/client/http/serializers/JsonSerializer.java
+++ codegraph-client-base/src/main/java/com/dnfeitosa/codegraph/client/http/serializers/JsonSerializer.java
@@ -1,10 +1,10 @@
 package com.dnfeitosa.codegraph.client.http.serializers;
 
-import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class JsonSerializer implements EntitySerializer {
 
     public String serialize(Object object) {
-        return new Gson().toJson(object);
+        return new GsonBuilder().setPrettyPrinting().create().toJson(object);
     }
 }

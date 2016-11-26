8bf7519dd9ea13877832d5c1735d0264a54c836f
Diego Feitosa <dnfeitosa@gmail.com>
2016-11-26 14:27:47 -0800

    adding logging

--- codegraph-db/src/main/java/com/dnfeitosa/codegraph/db/repositories/TypeRepository.java
+++ codegraph-db/src/main/java/com/dnfeitosa/codegraph/db/repositories/TypeRepository.java
@@ -2,20 +2,32 @@ package com.dnfeitosa.codegraph.db.repositories;
 
 
 import com.dnfeitosa.codegraph.db.nodes.TypeNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.neo4j.conversion.Result;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 @Repository
 public class TypeRepository {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TypeRepository.class);
+
+    private static int find = 0;
+    private static int hit = 0;
+    private static int missCache = 0;
+    private static int missMiss = 0;
     @Autowired
     private BaseTypeRepository baseRepository;
 
+    private Map<String, TypeNode> cache = new HashMap<>();
+
     public TypeNode save(TypeNode type) {
-        TypeNode existingType = baseRepository.findBySchemaPropertyValue("qualifiedName", type.getQualifiedName());
+        TypeNode existingType = findExisting(type);
         if (existingType != null) {
             type.setId(existingType.getId());
         }
@@ -26,6 +38,22 @@ public class TypeRepository {
         });
         type.getFields().forEach(f -> save(f.getType()));
         return baseRepository.save(type);
+    }
+
+    private TypeNode findExisting(TypeNode type) {
+        String qualifiedName = type.getQualifiedName();
+        if (cache.containsKey(qualifiedName)) {
+            LOGGER.info("-> hit {}", hit++);
+            return cache.get(qualifiedName);
+        }
+        LOGGER.info("-> find {}", find++);
+        TypeNode existing = baseRepository.findBySchemaPropertyValue("qualifiedName", qualifiedName);
+        if (existing != null) {
+            LOGGER.info(" -> miss cache {}", missCache++);
+            cache.put(qualifiedName, existing);
+        }
+        LOGGER.info("-> miss miss {}", missMiss++);
+        return existing;
     }
 
     public Result<TypeNode> findAll() {

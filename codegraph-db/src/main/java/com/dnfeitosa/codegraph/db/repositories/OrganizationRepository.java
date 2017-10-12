ea6b50be7d0bf272595127e132b4f22f01f9a49d
Diego Feitosa <dnfeitosa@gmail.com>
2017-10-12 16:42:24 -0700

    adds hierarchical "Organization" retrieval

---
+++ codegraph-db/src/main/java/com/dnfeitosa/codegraph/db/repositories/OrganizationRepository.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (C) 2015-2017 Diego Feitosa [dnfeitosa@gmail.com]
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.dnfeitosa.codegraph.db.repositories;
+
+import org.neo4j.ogm.model.Result;
+import org.neo4j.ogm.session.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toSet;
+import static java.util.stream.StreamSupport.stream;
+
+@Repository
+public class OrganizationRepository {
+
+    private Session session;
+
+    @Autowired
+    public OrganizationRepository(Session session) {
+        this.session = session;
+    }
+
+    public Set<String> getAllOrganizations() {
+        String query = " MATCH (x:Artifact) RETURN x.organization AS org " +
+            " UNION " +
+            " MATCH (x:Dependency) RETURN x.organization AS org ";
+        Result result = session.query(query, new HashMap<>());
+        return stream(result.spliterator(), false)
+            .map(r -> r.get("org").toString())
+            .collect(toSet());
+    }
+
+    public Set<String> getOrganizations(String parent) {
+        String query = " MATCH (x:Artifact) WHERE x.organization STARTS WITH {parent} RETURN x.organization AS org " +
+            " UNION " +
+            " MATCH (x:Dependency) WHERE x.organization STARTS WITH {parent} RETURN x.organization AS org ";
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("parent", parent + ".");
+        Result result = session.query(query, parameters);
+        return stream(result.spliterator(), false)
+            .map(r -> r.get("org").toString())
+            .collect(toSet());
+    }
+}
---
+++ codegraph-server/src/main/java/com/dnfeitosa/codegraph/server/services/Organization.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (C) 2015-2017 Diego Feitosa [dnfeitosa@gmail.com]
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.dnfeitosa.codegraph.server.services;
+
+import java.util.Objects;
+
+public class Organization {
+    private final String parent;
+    private final String name;
+
+    public Organization(String parent, String name) {
+        this.parent = parent;
+        this.name = name;
+    }
+
+    public String getParent() {
+        return parent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Organization that = (Organization) o;
+        return Objects.equals(parent, that.parent) &&
+            Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(parent, name);
+    }
+}

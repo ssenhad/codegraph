0aced3dcd9abe9ab8ad60efef9283e250e28eddf
Diego Feitosa <dnfeitosa@gmail.com>
2018-11-11 10:55:39 -0800

    add typescript dependencies and configuration, and migrate the entry point file syntax from JS to TS

---
+++ codegraph-viewer/frontend/app/index.tsx
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2015-2018 Diego Feitosa [dnfeitosa@gmail.com]
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* 3rd party imports */
+import 'bootstrap';
+
+import {HashRouter, Switch, Route} from 'react-router-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import './extensions';
+
+import Navbar from './components/navigation/navbar';
+import Artifacts from './modules/artifacts/artifacts';
+import Viewer from './modules/viewer/viewer';
+
+const _404 = () => {
+    return (
+        <span>Ooops... 404</span>
+    )
+};
+
+const Routes = () => (
+    <Switch>
+        <Route path="/artifacts" exact component={Artifacts} />
+        <Route path="/artifacts/:organization/:name" exact component={Artifacts} />
+        <Route path="/artifacts/:organization/:name/:version" component={Artifacts} />
+        <Route path="/viewer/:organization/:name/:version" component={Viewer} />
+        <Route component={_404} />
+    </Switch>
+);
+
+ReactDOM.render((
+    <HashRouter>
+        <React.Fragment>
+            <Navbar />
+            <Routes />
+        </React.Fragment>
+    </HashRouter>
+), document.getElementById("root"));
